56409123ac8cc846166c88ad2bc7716814bb500e
HyunseokLEE <unducklee@gmail.com>
2023-09-08 01:06:16 +0900

    feat: id & password

---
+++ index.js
@@ -0,0 +1,30 @@
+const title = document.querySelector("#alert-box");
+const titleOpenBtn = document.querySelector("#alert-open-btn");
+const titleCloseBtn = document.querySelector("#alert-close-btn");
+const idBtn = document.querySelector("#alert-id-btn");
+const passWordBtn = document.querySelector("#alert-password-btn");
+
+function handleOpenTitle(text) {
+  title.style.display = "block";
+  title.innerText = text;
+  titleCloseBtn.style.display = "inline";
+}
+
+function handleCloseTitle() {
+  title.style.display = "none";
+  titleCloseBtn.style.display = "none";
+}
+
+function handleIdBox(text) {
+  title.style.display = "block";
+  title.innerText = text;
+  titleCloseBtn.style.display = "inline";
+}
+
+function handlePassWordBox(text) {
+  title.style.display = "block";
+  title.innerText = text;
+  titleCloseBtn.style.display = "inline";
+}
+
+titleCloseBtn.addEventListener("click", handleCloseTitle);
