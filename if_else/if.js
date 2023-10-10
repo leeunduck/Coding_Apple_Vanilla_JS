4d40abf82f52f9f935884379f67788a62098177d
HyunseokLEE <unducklee@gmail.com>
2023-10-10 12:11:14 +0900

    feat: test score last

--- if_else/if.js
+++ if_else/if.js
@@ -1,19 +1,15 @@
-function game369(number) {
-  console.log(number);
-  console.log(typeof number);
-  if (number % 3 == 0) {
-    console.log("박수");
-  } else if (
-    number.slice(-1) == 3 ||
-    number.slice(-1) == 6 ||
-    number.slice(-1) == 9
-  ) {
-    console.log(typeof number);
-    console.log(number);
-    console.log("박수");
+function test(score1, score2) {
+  console.log(typeof score1, typeof score2);
+  console.log(score1, score2);
+  if (score1 < 0 || score1 > 100 || score2 < 0 || score2 > 100) {
+    alert("점수가 아닙니다");
+  } else if (score1 < 40 || score2 < 40) {
+    console.log("불합격");
+  } else if (score1 + score2 < 120) {
+    console.log("불합격");
   } else {
-    console.log("통과");
+    console.log("합격");
   }
 }
 
-game369(prompt("숫자"));
+test(Number(prompt("점수1")), Number(prompt("점수2")));
