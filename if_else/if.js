013c703e5e52ff53cdd6a158d9e4003246f17605
HyunseokLEE <unducklee@gmail.com>
2023-10-10 11:04:25 +0900

    feat: if/else 369 game upgrade

--- if_else/if.js
+++ if_else/if.js
@@ -1,6 +1,8 @@
 function game369() {
   const userNumber = prompt("숫자를 입력하세요");
-  if (userNumber % 3 == 0) {
+  if (userNumber % 9 == 0) {
+    console.log("박수 박수");
+  } else if (userNumber % 3 == 0) {
     console.log("박수");
   } else {
     console.log("통과");
