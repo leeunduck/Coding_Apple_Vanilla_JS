472c355573be2f1df0658b5e9ac70650507d05d6
HyunseokLEE <unducklee@gmail.com>
2023-10-10 11:52:04 +0900

    feat: 369 game last

--- if_else/if.js
+++ if_else/if.js
@@ -1,13 +1,19 @@
-function test(score1, score2) {
-  console.log(typeof score1, typeof score2);
-  console.log(score1, score2);
-  if (score1 < 40 || score2 < 40) {
-    console.log("불합격");
-  } else if (score1 + score2 < 120) {
-    console.log("불합격");
+function game369(number) {
+  console.log(number);
+  console.log(typeof number);
+  if (number % 3 == 0) {
+    console.log("박수");
+  } else if (
+    number.slice(-1) == 3 ||
+    number.slice(-1) == 6 ||
+    number.slice(-1) == 9
+  ) {
+    console.log(typeof number);
+    console.log(number);
+    console.log("박수");
   } else {
-    console.log("합격");
+    console.log("통과");
   }
 }
 
-test(Number(prompt("점수1")), Number(prompt("점수2")));
+game369(prompt("숫자"));
