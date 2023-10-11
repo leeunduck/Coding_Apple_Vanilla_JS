acf3a4e89143a4e5619358d8bebc2f49b14cf6ce
HyunseokLEE <unducklee@gmail.com>
2023-10-12 00:43:54 +0900

    feat: coffe problem (didn't solved)

--- Level2/index.js
+++ Level2/index.js
@@ -1,11 +1,6 @@
-var money = 10000;
-var 미래예금액 = 0;
+var first = 360;
+var total = 0;
 
-//여기에 코드짜보십시오
-if (money >= 50000) {
-  var 미래예금액 = money * 1.2 * 1.2;
-} else if (money < 50000) {
-  var 미래예금액 = money * 1.15 * 1.15;
-}
+total = first + (first * 2) / 3 + (((first * 2) / 3) * 2) / 3;
 
-console.log(미래예금액);
+console.log(total);
