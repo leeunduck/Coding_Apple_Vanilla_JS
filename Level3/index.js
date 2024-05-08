53a840a82b126bf70b9957b278cc365de1899719
HyunseokLEE <unducklee@gmail.com>
2024-05-09 02:26:07 +0900

    feat: array & object homework

---
+++ Level3/index.js
@@ -0,0 +1,7 @@
+const product = document.querySelector(".product");
+const price = document.querySelector(".price");
+
+let car2 = { name: "소나타", price: 50000 };
+
+product.innerHTML = car2.name;
+price.innerHTML = car2.price;
