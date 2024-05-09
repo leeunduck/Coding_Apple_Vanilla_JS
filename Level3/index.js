258b0fc389fbf14b393d05425a082f8b9175cbc2
이언덕 <110515401+leeunduck@users.noreply.github.com>
2024-05-09 22:58:02 +0900

    Level3/Lec4
    * feat: select

--- Level3/index.js
+++ Level3/index.js
@@ -1,7 +1,21 @@
-const product = document.querySelector(".product");
-const price = document.querySelector(".price");
+// const product = document.querySelector(".product");
+// const price = document.querySelector(".price");
 
-let car2 = { name: "소나타", price: [50000, 3000, 4000] };
+// let car2 = { name: "소나타", price: [50000, 3000, 4000] };
 
-product.innerHTML = car2.name;
-price.innerHTML = car2.price[0];
+// product.innerHTML = car2.name;
+// price.innerHTML = car2.price[0];
+
+const formSelect = document.querySelectorAll(".form-select");
+
+formSelect[0].addEventListener("input", handleSelect);
+
+function handleSelect(e) {
+  let value = e.currentTarget.value;
+
+  if (value == "셔츠") {
+    formSelect[1].classList.remove("form-hide");
+  } else {
+    formSelect[1].classList.add("form-hide");
+  }
+}
