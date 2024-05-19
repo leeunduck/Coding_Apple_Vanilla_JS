723aeea07fbc67e75b7b18dc6162875df3e81552
이언덕 <110515401+leeunduck@users.noreply.github.com>
2024-05-20 04:10:29 +0900

    Level3/lec8
    * feat: ajax button
    
    * feat: ajax js

--- Level3/list.js
+++ Level3/list.js
@@ -1,16 +1,35 @@
-const cardTitle = document.querySelectorAll(".title");
-const cardPrice = document.querySelectorAll(".price");
-
+const card = document.querySelector(".row");
+const plusBtn = document.querySelector("#more");
 let products = [
   { id: 0, price: 70000, title: "Blossom Dress" },
   { id: 1, price: 50000, title: "Springfield Shirt" },
   { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
-cardTitle[0].innerHTML = products[0].title;
-cardTitle[1].innerHTML = products[1].title;
-cardTitle[2].innerHTML = products[2].title;
+products.forEach((a, i) => {
+  let template = `<div class="col-sm-4">
+      <img src="https://via.placeholder.com/600" class="w-100">
+      <h5>${products[i].title}</h5>
+      <p>가격 : ${products[i].price}</p>
+    </div>`;
 
-cardPrice[0].innerHTML = `가격 : ${products[0].price}`;
-cardPrice[1].innerHTML = `가격 : ${products[1].price}`;
-cardPrice[2].innerHTML = `가격 : ${products[2].price}`;
+  card.insertAdjacentHTML("beforeend", template);
+});
+
+plusBtn.addEventListener("click", getGoods);
+
+function getGoods() {
+  fetch(`https://codingapple1.github.io/js/more1.json`)
+    .then((response) => response.json())
+    .then(function (data) {
+      data.forEach((a, i) => {
+        let template = `<div class="col-sm-4">
+            <img src="https://via.placeholder.com/600" class="w-100">
+            <h5>${data[i].title}</h5>
+            <p>가격 : ${data[i].price}</p>
+          </div>`;
+
+        card.insertAdjacentHTML("beforeend", template);
+      });
+    });
+}
