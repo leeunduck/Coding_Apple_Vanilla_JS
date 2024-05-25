202612740ff0f4f3862a95ee6dee9297cd6cf5d9
이언덕 <110515401+leeunduck@users.noreply.github.com>
2024-05-25 17:22:53 +0900

    Level3/lec11
    * feat: Ascending apply
    
    * feat: input filter apply

--- Level3/list.js
+++ Level3/list.js
@@ -1,12 +1,15 @@
 const card = document.querySelector(".row");
 const btnUnder = document.querySelector("#filter");
+const input = document.querySelector("#input");
 const plusBtn = document.querySelector("#more");
+const ascending = document.querySelector("#ascending");
 let products = [
   { id: 0, price: 70000, title: "Blossom Dress" },
   { id: 1, price: 50000, title: "Springfield Shirt" },
   { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
+/** 더보기 버튼 **/
 function appendProducts(products) {
   products.sort(function (a, b) {
     if (a.title < b.title) {
@@ -16,14 +19,12 @@ function appendProducts(products) {
     }
   });
 
-  card.innerHTML = "";
-
   products.forEach((product) => {
     let template = `<div class="col-sm-4">
         <img src="https://via.placeholder.com/600" class="w-100">
         <h5>${product.title}</h5>
         <p>가격 : ${product.price}</p>
-      </div>`;
+        </div>`;
     card.insertAdjacentHTML("beforeend", template);
   });
 }
@@ -58,9 +59,12 @@ function getGoods2() {
     });
 }
 
+/** input 값 비교 **/
 btnUnder.addEventListener("click", () => {
+  let inputPrice = input.value;
+
   let newProduct = products.filter(function (a) {
-    return a.price <= 60000;
+    return a.price <= inputPrice;
   });
 
   card.innerHTML = "";
@@ -70,6 +74,28 @@ btnUnder.addEventListener("click", () => {
         <img src="https://via.placeholder.com/600" class="w-100">
         <h5>${newProduct.title}</h5>
         <p>가격 : ${newProduct.price}</p>
+      </div>`;
+    card.insertAdjacentHTML("beforeend", template);
+  });
+});
+
+/** 가나다 정렬버튼 **/
+ascending.addEventListener("click", () => {
+  products.sort(function (a, b) {
+    if (a.title > b.title) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  card.innerHTML = "";
+
+  products.forEach((product) => {
+    let template = `<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${product.title}</h5>
+        <p>가격 : ${product.price}</p>
       </div>`;
     card.insertAdjacentHTML("beforeend", template);
   });
